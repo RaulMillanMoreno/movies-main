ff623d63d0713e4a90461db7f492dc442ad7c660
RaulMillanMoreno <raul.millan@sarria.salesians.cat>
2025-01-07 11:11:06 +0100

    aplico comentarios a api.dart y a actors_controller.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,5 +1,5 @@
-class Api {
-  static const baseUrl = "https://api.themoviedb.org/3/";
-  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
-  static const apiKey = "33b48ae4cc80ba5b4048937ff6930f3d";
+class Api {  
+  static const baseUrl = "https://api.themoviedb.org/3/";// URL base para acceder a la API de The Movie Database (TMDb)  
+  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";// URL base para acceder a las imágenes de TMDb  
+  static const apiKey = "33b48ae4cc80ba5b4048937ff6930f3d";// Clave de API utilizada para autenticar las solicitudes a la API de TMDb
 }
--- lib/controllers/actors_controller.dart
+++ lib/controllers/actors_controller.dart
@@ -3,34 +3,35 @@ import 'package:movies/api/api_service.dart';
 import 'package:movies/models/actor.dart';
 
 class ActorsController extends GetxController {
-  var isLoading = false.obs;
-  var mainTopRatedActors = <Actor>[].obs;
-  var watchListActors = <Actor>[].obs;
+  var isLoading = false.obs; // Indica si los datos están siendo cargados
+  var mainTopRatedActors = <Actor>[].obs; // Lista de actores más valorados
+  var watchListActors = <Actor>[].obs; // Lista de actores guardados en la lista de seguimiento
+
   @override
-  void onInit() async {
+  void onInit() async {// Se ejecuta al iniciar el controlador, se obtienen los actores más valorados    
     isLoading.value = true;
-    mainTopRatedActors.value = (await ApiService.getTopRatedActors())!;
+    mainTopRatedActors.value = (await ApiService.getTopRatedActors())!; // Obtiene los actores más valorados
     isLoading.value = false;
     super.onInit();
   }
-
-  bool isInActorsList(Actor actor) {
-    return watchListActors.any((m) => m.id == actor.id);
+  
+  bool isInActorsList(Actor actor) {// Verifica si un actor ya está en la lista de actores de seguimiento
+    return watchListActors.any((m) => m.id == actor.id); // Compara los IDs de los actores
   }
 
-  void addToActorsList(Actor actor) {
-    if (watchListActors.any((m) => m.id == actor.id)) {
+  void addToActorsList(Actor actor) {// Agrega o elimina un actor de la lista de seguimiento    
+    if (watchListActors.any((m) => m.id == actor.id)) {// Si el actor ya está en la lista, lo elimina
       watchListActors.remove(actor);
-      Get.snackbar('Success', 'removed from actors list',
-          snackPosition: SnackPosition.BOTTOM,
-          animationDuration: const Duration(seconds: 1),
-          duration: const Duration(seconds: 1));
-    } else {
+      Get.snackbar('Success', 'removed from actors list', // Muestra un mensaje de éxito
+        snackPosition: SnackPosition.BOTTOM,
+        animationDuration: const Duration(seconds: 1),
+        duration: const Duration(seconds: 1));
+    } else {// Si el actor no está en la lista, lo agrega      
       watchListActors.add(actor);
-      Get.snackbar('Success', 'added to actors list',
-          snackPosition: SnackPosition.BOTTOM,
-          animationDuration: const Duration(seconds: 1),
-          duration: const Duration(seconds: 1));
+      Get.snackbar('Success', 'added to actors list', // Muestra un mensaje de éxito
+        snackPosition: SnackPosition.BOTTOM,
+        animationDuration: const Duration(seconds: 1),
+        duration: const Duration(seconds: 1));
     }
   }
 }
