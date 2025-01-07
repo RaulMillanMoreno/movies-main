2ca5cf4b1b7045357c9e661456ed6666a37a1b2b
RaulMillanMoreno <raul.millan@sarria.salesians.cat>
2025-01-07 10:45:30 +0100

    Update api_end_points.dart

--- lib/api/api_end_points.dart
+++ lib/api/api_end_points.dart
@@ -1,6 +1,7 @@
 class ApiEndPoints {
-  static const products = "products";
-  static const popularMovies = "movie/popular";
-  static const upcomingMovies = "movie/upcoming";
-  static const getGenreList = "genre/movie/list";
+  
+  static const products = "products";// Constante que define el endpoint para obtener los productos  
+  static const popularMovies = "movie/popular";// Constante que define el endpoint para obtener las películas populares  
+  static const upcomingMovies = "movie/upcoming";// Constante que define el endpoint para obtener las películas próximas  
+  static const getGenreList = "genre/movie/list";// Constante que define el endpoint para obtener la lista de géneros de películas
 }
